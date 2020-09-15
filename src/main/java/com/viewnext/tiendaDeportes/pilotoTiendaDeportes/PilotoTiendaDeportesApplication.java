14b4ef38d957ddcf526fd6d6cced49e2c5da6dc3
vlopezalonso <vlopezalonso@gmail.com>
2020-09-15 11:55:59 +0200

    comm

---
+++ src/main/java/com/viewnext/tiendaDeportes/pilotoTiendaDeportes/PilotoTiendaDeportesApplication.java
@@ -0,0 +1,13 @@
+package com.viewnext.tiendaDeportes.pilotoTiendaDeportes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilotoTiendaDeportesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilotoTiendaDeportesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viewnext/tiendaDeportes/pilotoTiendaDeportes/PilotoTiendaDeportesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viewnext.tiendaDeportes.pilotoTiendaDeportes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilotoTiendaDeportesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
